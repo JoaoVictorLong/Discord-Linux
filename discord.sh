8899c36657b0298fbc0e50925ae154e3e0d78e3b
JoaoVictorLong <joao.vitor.longo@hotmail.com>
2024-11-07 20:59:51 -0300

    Add function to create schedule on systemd

--- discord.sh
+++ discord.sh
@@ -2,13 +2,36 @@
 
 my_version=$(dpkg-query -f '${Version}' -W 'discord' || echo "1")
 download(){
+	cd /etc/systemd/system/discord/
 	wget 'https://discord.com/api/download/stable?platform=linux&format=deb' -O discord
 	dpkg -i discord
 	rm discord
+	cd -
 }
 
 log(){
 	logger -t Discord -p local7.info "$1" 
+}
+
+create_systemd_time(){
+	mkdir /etc/systemd/system/discord
+	cat <<-EOT > /etc/systemd/system/discord/discord@.timer
+	[Unit]
+	Description=Discord upgrades
+
+	[Timer]
+	OnBootSec=1min
+	AccurancySec=1s
+	Unit=discord.target
+	EOT
+
+	cat <<-EOT > /etc/systemd/system//discord/discord.service
+	[Unit]
+	Description=Discord upgrades
+
+	DefaultDependencies=no
+	ExecStart=/etc/systemd/system/discord/discord.sh
+	EOT
 }
 
 if [[ $my_version == "1" ]]; then
